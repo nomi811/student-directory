4007f35101bb8ab637d7fe8850563879834b1281
Nomi Vos <nvoswork@gmail.com>
2016-06-24 04:44:36 -0500

    added lines to use CSV library.  Tweeked the puzzling problem with filename.  Changed variable in Save and Load methods.

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
+require 'csv'
 @students = []
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts '1. Input the students'
+  puts '2. Show the students'
+  puts '3. Save the list to students.csv'
+  puts '4. Load the list from students.csv'
+  puts '9. Exit'
 end
 
 def interactive_menu
   loop do
+    try_load_students
     print_menu
     process(STDIN.gets.chomp)
   end
@@ -17,31 +19,31 @@ end
 
 def process(selection)
   case selection
-  when "1"
+  when '1'
     input_students
-  when "2"
+  when '2'
     show_students
-  when "3"
+  when '3'
     save_students
-  when "4"
-    load_students(@filename)
-  when "9"
+  when '4'
+    load_students
+  when '9'
     exit
     puts "I don't know what you meant, try again"
   end
 end
 
 def create_students_array
-  @students << {name: @name, cohort: @cohort}
+  @students << { name: @name, cohort: @cohort }
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
 
   @name = STDIN.gets.chomp
 
-  while !@name.empty? do
+  until @name.empty?
     @name, @cohort = @name.split(',')
     create_students_array
     puts "Now we have #{@students.count} students"
@@ -57,8 +59,8 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
 
 def print_student_list
@@ -74,62 +76,62 @@ def print_footer
 end
 
 def save_students
-  puts "What file would you like to save?"
-  @filename = STDIN.gets.chomp
-  file = File.open(@filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  puts 'What file would you like to save?'
+  savefile = gets.chomp
+  savefile = 'students.csv' if savefile == ''
+  CSV.open(savefile, 'a+') do |csv|
+    @students.each do |student|
+      csv << [student[:name], student[:cohort]]
+    end
   end
-  puts "#{@students.length} students' data saved in #{@filename}"
+  puts "#{@students.length} students' data saved in #{savefile}"
   puts
   puts
-  file.close
 end
-
+=begin
 def read_file
-  file = File.open(@filename, "r")
-  file.readlines.each do |line|
-    @name, @cohort = line.chomp.split(',')
-    create_students_array
-  end
+  #file = CSV.read('students.csv')
+  #file = File.open(@filename, "r")
+  #file.each do |line|
+
+  #@name, @cohort = line.chomp.split(',')     #chomp.split(',')
+      #create_students_array
+  #end
+  puts file
 end
-
-def load_students(filename = @filename)
-  if @filename.nil?
-    puts "What file would you like to load?"
-    @filename = STDIN.gets.chomp
-    read_file
-
-  else
-    read_file
+=end
+def load_students(*)
+  puts 'What file would you like to load?'
+  loadfile = gets.chomp
+  #loadfile = 'students.csv' if loadfile == ''
+  CSV.foreach(loadfile) do |row|
+    @name, @cohort = row
+    # create_students_array
+    puts row.inspect
+    @students << { name: @name, cohort: @cohort }
   end
-  puts "#{@students.length} students' data loaded into #{@filename}"
+  puts "#{@students.length} students' data loaded into #{loadfile}"
   puts
   puts
 end
 
 def try_load_students
-  @filename = ARGV.first
-  if @filename.nil?
-    load_students(@filename = "students.csv")
+  filename = ARGV.first
+  return if filename.nil?
 
-  elsif File.exists?(@filename)
-    load_students(@filename)
-    puts "Loaded #{@students.count} from #{@filename}"
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
     puts
     puts
   else
-    puts "Sorry, #{@filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist."
     puts
     puts
     exit
   end
-
 end
 
-try_load_students
 interactive_menu
 
 
